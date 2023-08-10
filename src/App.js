709ebc127e4abb2d5222a0aaa3e7147b4536216c
tes-nat <tes.nat2023@gmail.com>
2023-08-10 21:18:26 +0300

    adding nano id

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react"
 import Die from "./Die"
-
+import {nanoid} from "nanoid"
 
 export default function App() {
 /**
- * Challenge: Create a `Roll Dice` button that will re-roll
- * all 10 dice
+ * Challenge: Update the array of numbers in state to be
+ * an array of objects instead. Each object should look like:
+ * { value: <random number>, isHeld: false }
  * 
- * Clicking the button should generate a new array of numbers
- * and set the `dice` state to that new array (thus re-rendering
- * the array to the page)
+ * Making this change will break parts of our code, so make
+ * sure to update things so we're back to a working state
  */
 
     const [dice, setDice] = React.useState(allNewDice())
@@ -17,7 +17,11 @@ export default function App() {
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            })
         }
         return newDice
     }
@@ -26,7 +30,9 @@ export default function App() {
         setDice(allNewDice())
     }
     
-    const diceElements = dice.map(die => <Die value={die} />)
+    const diceElements = dice.map(die => (
+        <Die key={die.id} value={die.value} />
+    ))
     
     return (
         <main>

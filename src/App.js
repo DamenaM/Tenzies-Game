b05a739636bc660299fbd393c9e11eec954dcfa3
tes-nat <tes.nat2023@gmail.com>
2023-08-10 21:58:20 +0300

    check each id

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import {nanoid} from "nanoid"
 
 export default function App() {
 /**
- * Challenge: Update the array of numbers in state to be
- * an array of objects instead. Each object should look like:
- * { value: <random number>, isHeld: false }
+ * Challenge: Create a function `holdDice` that takes
+ * `id` as a parameter. For now, just have the function
+ * console.log(id).
  * 
- * Making this change will break parts of our code, so make
- * sure to update things so we're back to a working state
+ * Then, figure out how to pass that function down to each
+ * instance of the Die component so when each one is clicked,
+ * it logs its own unique ID property. (Hint: there's more
+ * than one way to make that work, so just choose whichever
+ * you want)
+ * 
  */
 
     const [dice, setDice] = React.useState(allNewDice())
@@ -18,7 +22,7 @@ export default function App() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
             newDice.push({
-                value: Math.ceil(Math.random() * 6), 
+                value: Math.ceil(Math.random() * 6),
                 isHeld: false,
                 id: nanoid()
             })
@@ -30,8 +34,12 @@ export default function App() {
         setDice(allNewDice())
     }
     
+    function holdDice(id) {
+        console.log(id)
+    }
+    
     const diceElements = dice.map(die => (
-        <Die key={die.id} value={die.value} />
+        <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />
     ))
     
     return (
